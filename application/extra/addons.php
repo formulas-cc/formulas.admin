23337360b871c165c33d44abca8cd86510722ab6
zhangpengle <zhangpengle@formulas.cc>
2023-03-15 11:46:54 +0800

    init formulas.admin mpv v0.1.0

---
+++ application/extra/addons.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    'autoload' => false,
+    'hooks' => [
+        'upgrade' => [
+            'cms',
+        ],
+        'app_init' => [
+            'cms',
+        ],
+        'view_filter' => [
+            'cms',
+            'third',
+        ],
+        'user_sidenav_after' => [
+            'cms',
+            'vip',
+        ],
+        'xunsearch_config_init' => [
+            'cms',
+        ],
+        'xunsearch_index_reset' => [
+            'cms',
+        ],
+        'user_delete_successed' => [
+            'third',
+        ],
+        'user_logout_successed' => [
+            'third',
+        ],
+        'module_init' => [
+            'third',
+        ],
+        'action_begin' => [
+            'third',
+        ],
+        'config_init' => [
+            'third',
+        ],
+    ],
+    'route' => [
+        '/cms/$' => 'cms/index/index',
+        '/cms/t/[:diyname]$' => 'cms/tag/index',
+        '/cms/p/[:diyname]$' => 'cms/page/index',
+        '/cms/s$' => 'cms/search/index',
+        '/cms/d/[:diyname]$' => 'cms/diyform/index',
+        '/cms/d/[:diyname]/post' => 'cms/diyform/post',
+        '/cms/d/[:diyname]/[:id]' => 'cms/diyform/show',
+        '/cms/special/[:diyname]' => 'cms/special/index',
+        '/u/[:id]' => 'cms/user/index',
+        '/cms/[:diyname]$' => 'cms/channel/index',
+        '/cms/[:catename]/[:id]$' => 'cms/archives/index',
+        '/third$' => 'third/index/index',
+        '/third/connect/[:platform]' => 'third/index/connect',
+        '/third/callback/[:platform]' => 'third/index/callback',
+        '/third/bind/[:platform]' => 'third/index/bind',
+        '/third/unbind/[:platform]' => 'third/index/unbind',
+        [
+            'addon' => 'formulascc',
+            'domain' => 'www',
+            'rule' => [
+                '/' => 'formulascc/index/index',
+            ],
+        ],
+    ],
+    'priority' => [],
+    'domain' => '',
+];
---
+++ public/assets/js/addons.js
@@ -0,0 +1,29 @@
+define([], function () {
+    require.config({
+    paths: {
+        'jquery-colorpicker': '../addons/cms/js/jquery.colorpicker.min',
+        'jquery-autocomplete': '../addons/cms/js/jquery.autocomplete',
+        'jquery-tagsinput': '../addons/cms/js/jquery.tagsinput',
+        'clipboard': '../addons/cms/js/clipboard.min',
+    },
+    shim: {
+        'jquery-colorpicker': {
+            deps: ['jquery'],
+            exports: '$.fn.extend'
+        },
+        'jquery-autocomplete': {
+            deps: ['jquery'],
+            exports: '$.fn.extend'
+        },
+        'jquery-tagsinput': {
+            deps: ['jquery', 'jquery-autocomplete', 'css!../addons/cms/css/jquery.tagsinput.min.css'],
+            exports: '$.fn.extend'
+        }
+    }
+});
+
+if (Config.modulename === 'index' && Config.controllername === 'user' && ['login', 'register'].indexOf(Config.actionname) > -1 && $("#register-form,#login-form").length > 0 && $(".social-login").length == 0) {
+    $("#register-form,#login-form").append(Config.third.loginhtml || '');
+}
+
+});
